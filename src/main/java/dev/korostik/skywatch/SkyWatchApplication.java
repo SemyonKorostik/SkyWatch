4f1ed9f58f20202694804df27cb5ef6a07ee41ce
Semyon <fpm.korostik@bsu.by>
2026-07-26 21:08:07 +0300

    chore(gradle): initialize gradle project

---
+++ src/main/java/dev/korostik/skywatch/SkyWatchApplication.java
@@ -0,0 +1,13 @@
+package dev.korostik.skywatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyWatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyWatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/korostik/skywatch/SkyWatchApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.korostik.skywatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyWatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
